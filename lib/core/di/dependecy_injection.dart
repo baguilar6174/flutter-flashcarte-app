f037b6a25d4c222452d354a9f28317baac24c491
Bryan Aguilar <baguilar@technisys.com>
2025-07-13 21:07:42 -0500

    ✨ feat: Create domain layer, datasource flashcards and register with get it

--- lib/core/di/dependecy_injection.dart
+++ lib/core/di/dependecy_injection.dart
@@ -2,13 +2,21 @@ import 'package:get_it/get_it.dart';
 
 import 'package:flutter_flashcarte_app/features/profile/presentation/cubit/cubit.dart';
 
+import 'package:flutter_flashcarte_app/features/cards/domain/repositories/flashcard_repository.dart';
+import 'package:flutter_flashcarte_app/features/cards/domain/usecases/usecases.dart';
+
 GetIt sl = GetIt.instance;
 
 Future<void> serviceLocator() async {
   // dataSources
   // repositories
-  // use cases
+  _useCases();
   _cubit();
+}
+
+void _useCases() {
+  // Cards
+  sl.registerLazySingleton(() => CreateFlashcard(sl<FlashcardRepository>()));
 }
 
 void _cubit() {
